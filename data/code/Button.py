dfa24ff02faa94ef911736153c69954a3d9eaeb7
shaki <shakierdk@mail.ru>
2025-01-21 23:47:39 +0300

    (forgot to choose my branch) cleaning directory + code [part 2/2]

--- data/code/Button.py
+++ data/code/Button.py
@@ -1,5 +1,4 @@
-mport
-pygame
+import pygame
 
 
 class Button(pygame.sprite.Sprite):
@@ -20,7 +19,7 @@ class Button(pygame.sprite.Sprite):
         self.text = self.font.render(self.line_text, True, color_font)
         text_x = width // 2 - text.get_width() // 2
         text_y = height // 2 - text.get_height() // 2
-        screen.blit(self.text, (text_x, text_y))
+        self.surf.blit(self.text, (text_x, text_y))
 
     def check_cursor_position(self):
         return self.rect.collidepoint(pygame.mouse.get_pos())
@@ -32,19 +31,31 @@ class Button(pygame.sprite.Sprite):
 
 
 class MenuButton(Button):
-    def __init__(self, x, y, width, height, image_path, text, font_size, text_color=(0, 0, 0)):
+    def __init__(self, x, y, width, height, image_path, text, font_size, screen, text_color=(0, 0, 0)):
         super().__init__(x, y, width, height, text, text_color, font_size)
-        self.image = pygame.image.load(image_path)
-        self.image = pygame.transform.scale(self.image, (width, height))
-        self.rect = self.image.get_rect(topleft=(x, y))
-
+        self.line_text = text
         self.text = text
         self.text_color = text_color
         self.font_size = font_size
+        self.screen = screen
+
+        self.surf = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.rect = pygame.draw.rect(self.surf, (255, 255, 255, 0), x, y, width, height)
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (width, height))
         self.font = pygame.font.Font(None, font_size)
 
-    def update(self):
-        if self.text:
-            text_surface = self.font.render(self.text, True, self.text_color)
-            text_rect = text_surface.get_rect(center=self.rect.center)
-            self.image.blit(text_surface, text_rect)
+        self.text = self.font.render(self.line_text, True, self.text_color)
+        text_x = width // 2 - text.get_width() // 2
+        text_y = height // 2 - text.get_height() // 2
+
+        self.surf.blit(self.image, (0, 0))
+        self.surf.blit(self.text, (text_x, text_y))
+
+    def check_cursor_position(self):
+        return self.rect.collidepoint(pygame.mouse.get_pos())
+
+    def is_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            return self.check_cursor_position()
+        return False
