449fc3edd2ad6ae0eaf124957e243fdf309e8a70
shaki <shakierdk@mail.ru>
2025-01-27 18:31:20 +0300

    doing interface [part 2] (sshakie)

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main():
     screen = pygame.display.set_mode(size)
 
     money = 0
-    menu_window = Menu(screen, active=True)
-    shop_window = Shop(screen)
+    menu_window = Menu(screen)
+    shop_window = Shop(screen, active=True)
     game_window = Game(screen)
 
     running = True
@@ -41,7 +41,7 @@ class Menu:
         self.screen = screen
         self.active = active
         self.wallpaper = pygame.image.load('data/textures/wallpaper1 (hd).png')
-        self.title = pygame.image.load('data/textures/ui1.png')
+        self.title = pygame.image.load('data/textures/ui1.png').subsurface((0, 0, 200, 75))
         self.play_button = MenuButton(175, 250, 250, 62, 'data/textures/ui1.png', 'Играть', 50, crop=(3, 77, 250, 62))
         self.shop_button = MenuButton(175, 325, 250, 58, 'data/textures/ui1.png', 'Ларёк', 50, crop=(3, 155, 250, 58))
         self.settings_button = MenuButton(175, 400, 250, 58, 'data/textures/ui1.png', 'Настройки', 50,
@@ -50,7 +50,7 @@ class Menu:
 
     def render(self):
         self.screen.blit(self.wallpaper, (0, 0))
-
+        self.screen.blit(self.title, (200, 150))
         self.screen.blit(*self.play_button.get_rect_coord())
         self.screen.blit(*self.shop_button.get_rect_coord())
         self.screen.blit(*self.settings_button.get_rect_coord())
@@ -62,24 +62,28 @@ class Shop:
         self.screen = screen
         self.active = active
         self.wallpaper = pygame.image.load('data/textures/wallpaper2 (ai-hd).png')
+        self.separator1 = pygame.image.load('data/textures/ui1.png').subsurface((229, 505, 362, 32))
+        self.separator2 = pygame.image.load('data/textures/ui1.png').subsurface((229, 505, 362, 32))
 
-        self.mistake_upgrade = MenuButton(250, 300, 100, 25, 'data/textures/ui1.png', '-1 ошибка', 15,
+        self.mistake_upgrade = MenuButton(55, 136, 174, 102, 'data/textures/ui1.png', '-1 ошибка', 30,
                                           crop=(283, 129, 102, 57))
-        self.letter_upgrade = MenuButton(250, 375, 100, 25, 'data/textures/ui1.png', '+1 буква', 15,
+        self.letter_upgrade = MenuButton(240, 136, 174, 102, 'data/textures/ui1.png', '+1 буква', 30,
                                          crop=(283, 129, 102, 57))
-        self.game_upgrade = MenuButton(250, 450, 100, 25, 'data/textures/ui1.png', 'Игра', 15, crop=(283, 129, 102, 57))
+        self.game_upgrade = MenuButton(54, 244, 174, 102, 'data/textures/ui1.png', 'Игра', 30, crop=(283, 129, 102, 57))
 
-        self.button_custom = MenuButton(400, 300, 100, 25, 'data/textures/ui1.png', 'Кнопки', 15,
+        self.button_custom = MenuButton(93, 428, 174, 102, 'data/textures/ui1.png', 'Кнопки', 30,
                                         crop=(283, 129, 102, 57))
-        self.detail_custom = MenuButton(400, 375, 100, 25, 'data/textures/ui1.png', 'Детали', 15,
+        self.detail_custom = MenuButton(260, 428, 174, 102, 'data/textures/ui1.png', 'Детали', 30,
                                         crop=(283, 129, 102, 57))
-        self.letter_custom = MenuButton(400, 450, 100, 25, 'data/textures/ui1.png', 'Буквы', 15, )
-        self.background_custom = MenuButton(400, 525, 100, 25, 'data/textures/ui1.png', 'Фон', 15,
+        self.letter_custom = MenuButton(420, 428, 174, 102, 'data/textures/ui1.png', 'Буквы', 30,
+                                        crop=(283, 129, 102, 57))
+        self.background_custom = MenuButton(460, 550, 102, 70, 'data/textures/ui1.png', 'Фон', 30,
                                             crop=(283, 129, 102, 57))
 
     def render(self):
         self.screen.blit(self.wallpaper, (0, 0))
-
+        self.screen.blit(self.separator1, (36, 118))
+        self.screen.blit(self.separator2, (36, 414))
         self.screen.blit(*self.mistake_upgrade.get_rect_coord())
 
         self.screen.blit(*self.letter_upgrade.get_rect_coord())
