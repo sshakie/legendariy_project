1470d873f4485be9a2e831193d3df613d32fe824
yymato <070913a@gmail.com>
2025-01-28 18:25:42 +0300

    Вернул кнопки в game потерянные из-за ошибки слияния веток

--- data/code/game/game.py
+++ data/code/game/game.py
@@ -16,6 +16,11 @@ class Game:
         self.count_string = 0  # Порядковый номер попытки
         self.input_word = str()
 
+        self.exit_button = KeyboardButton(8, 6, 134, 39, (255, 255, 255), 'выйти', border_radius=2)
+        self.line = pygame.image.load('data/textures/ui.png').subsurface((0, 557, 601, 93))
+        self.attempts = 5
+        self.font = pygame.font.Font(None, 50)
+
         # Клавиатура
         self.keyboard = []  # TODO Сделать клавиатуру
 
@@ -30,8 +35,10 @@ class Game:
 
     def render(self):
         self.screen.blit(self.wallpaper, (0, 0))
-        attempt_label = self.font.render(f'Попыток: {self.attempts}', True, (255, 255, 255))
-        self.screen.blit(attempt_label, (100, 50))
+        attempt_label = self.font.render(f'Попыток: {self.attempts}', True, (0, 0, 0))
+        self.screen.blit(attempt_label, (289, 52))
+        self.screen.blit(*self.exit_button.get_rect_coord())
+        self.screen.blit(self.line, (0, 543))
         for i in range(len(self.guessing)):
             for q in self.guessing[i].keys():
                 self.screen.blit(*self.guessing[i][q].get_rect_coord())
