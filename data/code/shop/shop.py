0d8cf2bd98f4839166abe1a3b607f2d53b033d39
shaki <shakierdk@mail.ru>
2025-02-06 21:30:31 +0300

    cleaning code [part 1/2]

--- data/code/shop/shop.py
+++ data/code/shop/shop.py
@@ -4,33 +4,33 @@ from data.code.animating import AnimatedSprite
 
 
 class Shop:
-    def __init__(self, screen, money, mistake, letter, active=False):
-        self.screen = screen
+    def __init__(self, screen, money, mistake_goods, letter_goods, active=False):
         self.active = active
-        self.money = money
-        self.mistakes = mistake
-        self.letters = letter
+        self.screen = screen
         self.text_font = 'data/myy.ttf'
         self.font = pygame.font.Font(self.text_font, 30)
         self.clock = pygame.time.Clock()
+
+        self.money = money
+        self.mistake_goods = mistake_goods
+        self.letter_goods = letter_goods
         self.fps = 60
 
-        # Интерфейс
-        self.all_sprites = pygame.sprite.Group()
+        # Интерфейс (основное)
+        self.all_sprites = pygame.sprite.Group()  # для анимированного фона
         self.wallpaper = AnimatedSprite(load_image('data/textures/wallpapers/animated-wallpaper2.png'), 60, 1, 0, 0,
                                         self.all_sprites)
-        self.no_problems = pygame.image.load('data/textures/ui.png').subsurface((697, 0, 600, 800))
-        self.coin = pygame.image.load('data/textures/ui.png').subsurface((594, 0, 33, 30))
-        self.separator1 = pygame.image.load('data/textures/ui.png').subsurface((0, 115, 600, 40))
-        self.separator2 = pygame.image.load('data/textures/ui.png').subsurface((0, 115, 600, 40))
+        self.sublayer = pygame.image.load('data/textures/ui.png').subsurface((697, 0, 600, 800))
+        self.separator = pygame.image.load('data/textures/ui.png').subsurface((0, 115, 600, 40))
         self.upgrade_text = pygame.image.load('data/textures/ui.png').subsurface((299, 0, 294, 57))
         self.customization_text = pygame.image.load('data/textures/ui.png').subsurface((299, 58, 335, 56))
-        self.money_label = self.font.render(str(self.money), True, (100, 0, 0))
-        self.mistake_count = self.font.render(str(self.mistakes), True, (100, 0, 0))
-        self.letter_count = self.font.render(str(self.letters), True, (100, 0, 0))
+
+        self.coin = pygame.image.load('data/textures/ui.png').subsurface((594, 0, 33, 30))
         self.prices = pygame.image.load('data/textures/prices.png')
 
-        # Кнопки
+        # Интерфейс (кнопки)
+        self.exit_button = Button(46, 657, 509, 75, 'назад', 0, type=6)
+
         self.mistake_upgrade = Button(29, 197, 134, 75, ['право на ', 'ошибку'], 22, type=4)
         self.letter_upgrade = Button(189, 196, 134, 75, ['раскрыть ', 'букву'], 22, type=4)
         self.game_upgrade = Button(29, 277, 134, 75, ['игра-', 'капча'], 26, type=4, offset=(-10, 0))
@@ -38,22 +38,22 @@ class Shop:
         self.detail_custom = Button(271, 427, 134, 75, 'детали', 27, type=4)
         self.letter_custom = Button(431, 427, 134, 75, 'буквы', 30, type=4)
         self.background_custom = Button(472, 512, 86, 59, 'фон', 30, type=5, offset=(0, -10))
-        self.exit_button = Button(46, 657, 509, 75, 'назад', 0, type=6)
 
-        # Звуки
-        self.sfx_select = pygame.mixer.Sound('data/sounds/select.wav')
-        self.sfx_select2 = pygame.mixer.Sound('data/sounds/select_2.wav')
+    def render(self):  # Функция для отображения интерфейса
+        money_label = self.font.render(str(self.money), True, (100, 0, 0))
+        mistake_label = self.font.render(str(self.mistake_goods), True, (100, 0, 0))
+        letter_label = self.font.render(str(self.letter_goods), True, (100, 0, 0))
 
-    def render(self):  # Функция для рендера интерфейса
-        self.all_sprites.draw(self.screen)
+        self.all_sprites.draw(self.screen)  # анимированный фон
         self.all_sprites.update()
-        self.screen.blit(self.no_problems, (0, 0))
-        self.screen.blit(self.coin, (551, 17))
-        self.screen.blit(self.money_label, (545 - self.money_label.get_width(), 24))
-        self.screen.blit(self.separator1, (0, 160))
-        self.screen.blit(self.separator2, (0, 390))
+        self.screen.blit(self.sublayer, (0, 0))
+
         self.screen.blit(self.upgrade_text, (143, 119))
+        self.screen.blit(self.separator, (0, 160))
         self.screen.blit(self.customization_text, (125, 360))
+        self.screen.blit(self.separator, (0, 390))
+
+        self.screen.blit(*self.exit_button.get_rect_coord())
         self.screen.blit(*self.mistake_upgrade.get_rect_coord())
         self.screen.blit(*self.letter_upgrade.get_rect_coord())
         self.screen.blit(*self.game_upgrade.get_rect_coord())
@@ -61,10 +61,13 @@ class Shop:
         self.screen.blit(*self.detail_custom.get_rect_coord())
         self.screen.blit(*self.letter_custom.get_rect_coord())
         self.screen.blit(*self.background_custom.get_rect_coord())
-        self.screen.blit(*self.exit_button.get_rect_coord())
+
+        self.screen.blit(self.coin, (551, 17))
         self.screen.blit(self.prices, (0, 0))
-        self.screen.blit(self.mistake_count, (44, 245))
-        self.screen.blit(self.letter_count, (204, 245))
+        self.screen.blit(money_label, (545 - money_label.get_width(), 24))
+        self.screen.blit(mistake_label, (44, 245))
+        self.screen.blit(letter_label, (204, 245))
+
         self.clock.tick(self.fps)
 
     def on_click(self, event):  # Функция нажатия кнопки
@@ -82,15 +85,6 @@ class Shop:
                            self.background_custom, self.exit_button]:
                 if button.selecting():
                     if button == self.exit_button:
-                        self.sfx_select2.play()
+                        pygame.mixer.Sound('data/sounds/select_2.wav').play()
                     else:
-                        self.sfx_select.play()
-
-    def update(self, money, mistake, letter):  # Подгрузка данных из main для обновления
-        self.money = money
-        self.mistakes = mistake
-        self.letters = letter
-
-        self.money_label = self.font.render(str(self.money), True, (100, 0, 0))
-        self.mistake_count = self.font.render(str(self.mistakes), True, (100, 0, 0))
-        self.letter_count = self.font.render(str(self.letters), True, (100, 0, 0))
+                        pygame.mixer.Sound('data/sounds/select.wav').play()
